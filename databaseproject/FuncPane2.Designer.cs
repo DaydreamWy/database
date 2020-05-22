aac6f364b20d2bd83dfb2eaba416944344762014
KenelmQLH <1097824882@qq.com>
2020-05-22 17:02:19 +0800

    增加了页面，看起来变得有点诡异

--- databaseproject/FuncPane2.Designer.cs
+++ databaseproject/FuncPane2.Designer.cs
@@ -28,18 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnIN = new System.Windows.Forms.Button();
+            this.BtnOUT = new System.Windows.Forms.Button();
+            this.ProductIOList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductIOList)).BeginInit();
             this.SuspendLayout();
             // 
-            // FuncPanl2
+            // BtnIN
+            // 
+            this.BtnIN.Location = new System.Drawing.Point(45, 23);
+            this.BtnIN.Name = "BtnIN";
+            this.BtnIN.Size = new System.Drawing.Size(146, 49);
+            this.BtnIN.TabIndex = 0;
+            this.BtnIN.Text = "入库";
+            this.BtnIN.UseVisualStyleBackColor = true;
+            this.BtnIN.Click += new System.EventHandler(this.BtnIN_Click);
+            // 
+            // BtnOUT
+            // 
+            this.BtnOUT.Location = new System.Drawing.Point(484, 23);
+            this.BtnOUT.Name = "BtnOUT";
+            this.BtnOUT.Size = new System.Drawing.Size(138, 49);
+            this.BtnOUT.TabIndex = 2;
+            this.BtnOUT.Text = "出库";
+            this.BtnOUT.UseVisualStyleBackColor = true;
+            this.BtnOUT.Click += new System.EventHandler(this.BtnOUT_Click);
+            // 
+            // ProductIOList
+            // 
+            this.ProductIOList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ProductIOList.Location = new System.Drawing.Point(45, 107);
+            this.ProductIOList.Name = "ProductIOList";
+            this.ProductIOList.RowTemplate.Height = 27;
+            this.ProductIOList.Size = new System.Drawing.Size(577, 312);
+            this.ProductIOList.TabIndex = 3;
+            // 
+            // FuncPane2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "FuncPanl2";
+            this.Controls.Add(this.ProductIOList);
+            this.Controls.Add(this.BtnOUT);
+            this.Controls.Add(this.BtnIN);
+            this.Name = "FuncPane2";
             this.Size = new System.Drawing.Size(668, 472);
+            this.Load += new System.EventHandler(this.FuncPane2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductIOList)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnIN;
+        private System.Windows.Forms.Button BtnOUT;
+        private System.Windows.Forms.DataGridView ProductIOList;
     }
 }
--- databaseproject/FuncPane2.cs
+++ databaseproject/FuncPane2.cs
@@ -15,5 +15,30 @@ namespace databaseproject
         {
             InitializeComponent();
         }
+
+        private void BtnIN_Click(object sender, EventArgs e)
+        {
+            Form4 InProductCard = new Form4();
+            InProductCard.Owner = this.ParentForm;
+            InProductCard.Show();
+
+        }
+
+        private void BtnOUT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FuncPane2_Load(object sender, EventArgs e)
+        {
+            DataSet ds;
+            string sqltmp = "select * from Products,Shelf where Shelf.p_id = Products.p_id and s_id = '" + 
+                    Form1.LoginUserId+"'";
+
+            ds = SqlFunc.Query(sqltmp);            
+
+            this.ProductIOList.DataSource = ds.Tables[0];
+            //MessageBox.Show("get " + ds.Tables[0].Rows.Count.ToString() + " records!");
+        }
     }
 }
